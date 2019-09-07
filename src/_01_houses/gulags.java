1debc06163d7179cb5ae62549cfb1df2f5a3adb5
league <league@imac-17.attlocal.net>
2019-09-07 13:01:00 -0700

    finish one only

--- src/_01_houses/gulags.java
+++ src/_01_houses/gulags.java
@@ -1,5 +1,6 @@
 package _01_houses;
 
+import java.awt.Color;
 import java.util.Random;
 
 import org.jointheleague.graphical.robot.Robot;
@@ -13,30 +14,68 @@ public class gulags {
 		rob.setX(40);
 		rob.setSpeed(1000);
 		rob.penDown();
-		for(int i=0; i<9; i++) {
+		for(int i=0; i<=10; i++) {
 	   Random rand = new Random();
-		drawflathouse();
+	   int fish = rand.nextInt(3);
+	   if(fish==0) {
+	    drawpeakhouse("small", Color.BLACK);
+	   }
+	   else if(fish==1) {
+		    drawpeakhouse("meduim", Color.white);
+		   }
+	   else if(fish==2) {
+		    drawflathouse("large", Color.red);
+		   }
 		}
 	}
-	void drawpeakhouse() {
-		rob.move(40);
+	static void drawpeakhouse(String height, Color lgbdq) {
+		rob.setPenColor(lgbdq);
+		int kaiserwilhelm2 = 0;
+		if(height.equals("small")) {
+			kaiserwilhelm2 = 60;	
+			}
+			else if(height.equals("medium")) {
+			kaiserwilhelm2 = 120;
+			}
+			else if(height.equals("large")) {
+			kaiserwilhelm2 = 250;
+			}
+			else {
+				kaiserwilhelm2 = 200;
+			}
+		rob.move(kaiserwilhelm2);
 		rob.turn(30);	
 		rob.move(25);	
-		rob.turn(120);		
+		rob.turn(120);
 		rob.move(25);
-		rob.turn(30);	
-		rob.move(40);
+		rob.turn(30);
+		rob.move(kaiserwilhelm2);
+		rob.turn(-90);
+		rob.move(25);
 		rob.turn(270);
-		rob.move(25);
 	}
 	
 	
-static void drawflathouse(int height) {
-	rob.move(100);
+static void drawflathouse(String height, Color lgbdq) {
+	rob.setPenColor(lgbdq);
+	int kaiserwilhelm2 = 0;
+	if(height.equals("small")) {
+	kaiserwilhelm2 = 60;	
+	}
+	else if(height.equals("medium")) {
+	kaiserwilhelm2 = 120;
+	}
+	else if(height.equals("large")) {
+	kaiserwilhelm2 = 250;
+	}
+	else {
+		kaiserwilhelm2 = 200;
+	}
+	rob.move(kaiserwilhelm2);
 	rob.turn(90);
 	rob.move(25);
 	rob.turn(90);
-	rob.move(100);
+	rob.move(kaiserwilhelm2);
 	rob.turn(-90);
 	rob.move(25);
 	rob.turn(270);
