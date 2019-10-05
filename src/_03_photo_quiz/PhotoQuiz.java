1127c50863291641477ae86881fa52195a2e4ba4
league <league@imac-22.attlocal.net>
2019-10-05 12:59:15 -0700

    this was easy but fun.

--- src/_03_photo_quiz/PhotoQuiz.java
+++ src/_03_photo_quiz/PhotoQuiz.java
@@ -15,8 +15,13 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
 public class PhotoQuiz {
-
+	 
 	public void run() throws Exception {
+
+	 
+
+
+		
 
 		JFrame quizWindow = new JFrame();
 		quizWindow.setVisible(true);
@@ -26,44 +31,56 @@ public class PhotoQuiz {
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
 		// “Copy Image Address”)
-
+		String china = "download.jpg";
 		// 2. create a variable of type "Component" that will hold your image
-
+		Component ussr;
 		// 3. use the "createImage()" method below to initialize your Component
-
+		ussr = createImage("https://vignette.wikia.nocookie.net/pewdiepie/images/5/51/Water_Sheep.png/revision/latest?cb=20190826022918");
 		// 4. add the image to the quiz window
-
+		quizWindow.add(ussr);
 		// 5. call the pack() method on the quiz window
-
+		quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+		String germany = JOptionPane.showInputDialog(null, "has water sheep died in a pewdiepie video?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+		if(germany.equals(germany)) {
+		System.out.println("correct");
+		}
 		// 8. print "INCORRECT" if the answer is wrong
-
+		else {
+		System.out.println("incorrect");
+		}
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
-
+		quizWindow.remove(ussr);
 		// 10. find another image and create it (might take more than one line
 		// of code)
+		Component nsgwp;
+		nsgwp = createImage("https://vignette.wikia.nocookie.net/pewdiepie/images/e/ef/Sven-mc.png/revision/latest/scale-to-width-down/180?cb=20190922220753");
+		
 
 		// 11. add the second image to the quiz window
-
+		quizWindow.add(nsgwp);
 		// 12. pack the quiz window
-
+		quizWindow.pack();
 		// 13. ask another question
-
+		String unitednations = JOptionPane.showInputDialog("when did pewdiepie get sven?");
 		// 14+ check answer, say if correct or incorrect, etc.
-
-	}
-
+			if(unitednations.equals(unitednations)) {
+			System.out.println("correct");
+			}
+			else {
+			System.out.println("incorrect");	
+			}
+		}
+ 
 	private Component createImage(String imageUrl) throws MalformedURLException {
 		URL url = new URL(imageUrl);
 		Icon icon = new ImageIcon(url);
 		JLabel imageLabel = new JLabel(icon);
 		return imageLabel;
 	}
-
+ 
 	/* OPTIONAL */
 	// *14. add scoring to your quiz
 	// *15. make something happen when mouse enters image
