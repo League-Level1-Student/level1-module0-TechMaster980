822015058cf4d112ab0781aa5480e167bf613895
league <league@imac-37.attlocal.net>
2019-11-16 12:50:55 -0800

    finished one

--- src/_07_fortune_cookie/fortunecookie.java
+++ src/_07_fortune_cookie/fortunecookie.java
@@ -1,17 +1,57 @@
 package _07_fortune_cookie;
 
-import javax.swing.JButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
-public class fortunecookie {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class fortunecookie implements ActionListener {
 	 public void showButton() {
-         System.out.println("Button clicked");
+         
+		 JFrame frame = new JFrame();
+			JPanel panel = new JPanel();
+		 
+		 System.out.println("Button clicked");
    
-       JFrame frame = new JFrame();
+    
+
 		frame.setVisible(true);
          JButton button = new JButton();
          frame.add(button);
          frame.pack();
          button.addActionListener(this);
+         
+     
+	 
+	 
+	 
 	 }
-	
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "wwwwwwwwwwwwwwwwwwwwwwwoooooooooooooooooooooooohhhhhhhhhhhooooooooooooooooooooooooooooooooooooooooooooooooooo");
+	    
+        int rand = new Random().nextInt(5);
+        if(rand == 0) {
+        JOptionPane.showMessageDialog(null,"henry sucks");
+        }
+        if(rand == 1) {
+        JOptionPane.showMessageDialog(null,"china");
+        }
+        if(rand == 2) {
+        JOptionPane.showMessageDialog(null,"america is winning trade war");
+        }
+        if(rand == 3) {
+        JOptionPane.showMessageDialog(null,"russia");
+        }
+        if(rand == 4) {
+        JOptionPane.showMessageDialog(null,"henry is the fattest person in the world");
+        }
+        
+	}
 }
