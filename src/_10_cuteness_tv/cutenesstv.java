5ce6175cc1354ca4f06bc6e6fda3908beaab0f43
league <league@imac-37.attlocal.net>
2019-12-28 12:55:37 -0800

    tried my best

--- src/_10_cuteness_tv/cutenesstv.java
+++ src/_10_cuteness_tv/cutenesstv.java
@@ -2,71 +2,51 @@ package _10_cuteness_tv;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.URI;
+import java.util.Random;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 public class cutenesstv implements ActionListener {
-	void henryisthenewfatalbertcalledit() {
-	 JFrame frame = new JFrame();
-		JPanel panel = new JPanel();
-		frame.setVisible(true);
- JButton button = new JButton();
- frame.add(button);
- frame.pack();
- button.addActionListener(this);
-	frame.setVisible(true);
-JButton button1 = new JButton();
-frame.add(button);
-frame.pack();
-button.addActionListener(this);	frame.setVisible(true);
-JButton button2 = new JButton();
-frame.add(button);
-frame.pack();
-button.addActionListener(this);	frame.setVisible(true);
-JButton button3 = new JButton();
-frame.add(button);
-frame.pack();
-button.addActionListener(this);	frame.setVisible(true);
-JButton button4 = new JButton();
-frame.add(button);
-frame.pack();
-button.addActionListener(this);	frame.setVisible(true);
-JButton button5 = new JButton();
+void henryisthenewfatalbertcalledit() {
+JFrame frame = new JFrame();
+System.out.println("button clicked");
+frame.setVisible(true);
+JButton button = new JButton();
 frame.add(button);
 frame.pack();
 button.addActionListener(this);
-	}
-void cutevideo() {
-	playVideo("https://www.youtube.com/watch?v=LNz8pWUKkYo");
 }
 private void playVideo(String string) {
-	// TODO Auto-generated method stub
-	}
-void cutevideo1() {
-	playVideo("https://www.youtube.com/watch?v=rTlcfyDRFXk");
-}
-void happyvideo(){
-    playVideo("https://www.youtube.com/watch?v=ih8PWBHlbws");
-}
-void thisvideowillmakeyousmile( ) {
-playVideo("https://www.youtube.com/watch?v=X-bogN0V8RM");
-}
-void bestdayever() {
-playVideo("https://www.youtube.com/watch?v=fpNQsUQy95c");
-}
-void pufferfish() {
-	playVideo("https://www.youtube.com/watch?v=wuB9gdWstEg");
+// TODO Auto-generated method stub
 }
 void playVideo() {
-   try {
-         URI uri = new URI("videoID");
-         java.awt.Desktop.getDesktop().browse(uri);
-    } catch (Exception e) {
-         e.printStackTrace();
-    }
+try {
+URI uri = new URI("videoID");
+java.awt.Desktop.getDesktop().browse(uri);
+}catch (Exception e){
+e.printStackTrace();
+}
 }
 @Override
-public void actionPerformed(ActionEvent e) {
-	// TODO Auto-generated method stub
-	}
+public void actionPerformed(ActionEvent e){
+// TODO Auto-generated method stub
+}
+int rand = new Random().nextInt(3);
+void showDucks() {
+if(rand == 0) {	
+playVideo("https://www.youtube.com/watch?v=MtN1YnoL46Q");
+}
+}
+void showFrog() {
+if(rand == 1) {
+playVideo("https://www.youtube.com/watch?v=cBkWhkAZ9ds");
+}
+}
+void showFluffyUnicorns() {
+if(rand == 2) {
+playVideo("https://www.youtube.com/watch?v=a-xWhG4UU_Y");
+}
+}
+public void showButton() {
+// TODO Auto-generated method stub
+}
 }
--- src/_10_cuteness_tv/cutenesstvrunner.java
+++ src/_10_cuteness_tv/cutenesstvrunner.java
@@ -1,7 +1,6 @@
 package _10_cuteness_tv;
-
 public class cutenesstvrunner {
 public static void main(String[] args) {
-		
-	}
+new cutenesstv().showButton();
+}
 }
